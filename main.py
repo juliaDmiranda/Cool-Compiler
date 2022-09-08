9ca639a2419f4835aca4ed30cf2e470335fb5757
Júlia Miranda Rodrigues <60434126+juliaDmiranda@users.noreply.github.com>
2022-09-08 11:38:55 -0300

    j

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-import lexicalAnalyzer as LA
 import sys
 
-fileName    =  sys.argv[1]
+def printProgramElements():
+    '''
+    Prints each token of the program followed by theirs identifiers  
+    '''
+    pass
 
-tokens = LA.readNtokenize(fileName)
+def classifyTokens(line):
+    '''
+    Function that receive a line of a Cool program and classify each token of that line
+    '''
+    pass
 
-LA.printTokens(tokens)
+def readProgram(fileName):
+    '''
+    Function to read the program in Cool and store in a list wich elements are lines of code
+    '''
+    code = []
+    with open(fileName, 'r') as file:    
+        line = file.readlines()
+
+        print(line)
+        '''
+        Considering that the grammer do not make a 
+        restriction about space where you can use 
+        it or not between symbols, I must check if
+        after split strings, there are any digit or special symbol on it
+        My be use regex would make it easier
+        '''
+
+
+fileName    = sys.argv[1]
+
+identifier = [] # list of dictionary. Think in classify each line in a list to mark the numeration of the line in one take
+
+readProgram(fileName)
 
