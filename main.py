cca6e9f8c1bffe9eb3005999e8525be8b43a0004
Júlia Miranda Rodrigues <juliamrz2@outlook.com>
2022-10-14 16:18:35 -0300

    modifica main
    Algumas modificações convenientes para teste

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-import lexicalAnalyzer as LA
 import sys
-
-fileName    =  sys.argv[1]
-
+import lexicalAnalyzer as LA
+import syntaxAnalyzer as SA
+from class_ import Token
+def printDiferente(aux):
+    print(next(aux))
+try:
+    fileName    =  sys.argv[1]
+except:
+    fileName      =  "primesHard.cl"
 tokens = LA.readNtokenize(fileName)
-
 LA.printTokens(tokens)
+try:
+    l = []
+    for i in tokens:
+        for j in i:
+            l.append(j)
+
+    SA.switchTokens(l)
+    
+except StopIteration:
+    print("Erro amigo")
