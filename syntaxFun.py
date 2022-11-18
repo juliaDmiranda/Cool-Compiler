3cc8acb46cb7f3c07afdece4773e14ef9a7adac5
Júlia Miranda Rodrigues <juliamrz2@outlook.com>
2022-11-17 21:09:45 -0300

    Adiciona função ID_func

--- syntaxFun.py
+++ syntaxFun.py
@@ -35,7 +35,46 @@ def ATT_func(data):
     data = expr_line(data) # recursão à esquerda
 
     return data
+def ID_func(data):
+    """
+    SOBRE
+    -------------
+    Função para tratar expressões que começam com ID. Trata-se de uma expressão com recusão a direita.
+    ID --> expr 
+    ID --> epslon, isto é, somente o ID
 
+    PARÂMETROS
+    -------------
+    data: lista que contém classe de manipulação de tokens, lista de tipos e árvore semântica
+
+    RETORNO
+    -------------
+    - data: lista que contém classe de manipulação de tokens, lista de tipos e árvore semântica modificados
+    
+    FORMAÇÃO DA ÁRVORE SEMÂNTICA
+    ----------------------------
+    Na estrutura da árvore semântica, uma expressão inicializada com ID gerará 
+    um nó somente na outra função na qual foi chamada. Caso contrário geraria 
+    redundância de nó
+                    (IF)           1 raiz
+                /    |     \\
+            expr1  expr2  expr3    3 filhos
+    
+    """
+    # adicionar raiz
+    data[0].nexToken(PC.SIG.TokenFound)
+    if(data[0].situation == PC.SIG.EndOfProgram): return data # return data
+    
+    # <-
+    if(data[0].token.idEqual(Ids.ATT_ID)): # Verifica Atribuição
+        data = ATT_func(data)
+    # ()
+    elif(data[0].token.idEqual(Ids.O_PARENTHESIS)):
+        data = O_PARENTHESIS_func(data)
+    
+    # sozinho "ID" --> não faz nada
+
+    return data
 def IF_func(data):
     """
     SOBRE
