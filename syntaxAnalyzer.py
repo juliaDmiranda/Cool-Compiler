3c8cb664f5f065cbd847b904c9080646710bbfec
Júlia Miranda Rodrigues <juliamrz2@outlook.com>
2022-12-08 17:52:15 -0300

    adiciona método para printar árvore
    nesse método a árvore pode ser mstrada ou gravada em um arquivo

--- syntaxAnalyzer.py
+++ syntaxAnalyzer.py
@@ -1,6 +1,28 @@
-import class_
+import os
 import syntaxFun as SF
-import Program_class as PC
+import synTree as st
+# import syntaxFun_bf as SF
+
+def showTree_aux(myTree:st.Node, file, print_ = False):
+    try:
+        if(type(myTree) != list):
+            if(print_):
+                print(myTree)
+            file.write("\n"+str(myTree))
+            if(type(myTree) == st.Node):
+                if(myTree.children != None):
+                    if(len(myTree.children) != 0):
+                        for child in myTree.children:
+                            file = showTree_aux(child, file)   
+        return file
+    except Exception as ex:
+        print(ex.args)
+        print("\n\n\n>>>>>>>>>>>>>",myTree.name)
+
+def showTree(root, print_ = False):
+    file = open("synTree", "w")
+    for c in root:
+        file = showTree_aux(c, file, print_)
 
 def showErrors(err):
     aux = [a for a in  err if not a==[]]
@@ -10,6 +32,8 @@ def showErrors(err):
         print("Syntax Error\n"+('-'*30)+"\n")
         for e in aux:
             print(e)
+    os.system("PAUSE")
+    os.system("CLS")
 
 def switchTokens(l):
     if(l == []):
@@ -17,5 +41,10 @@ def switchTokens(l):
     # primeira chamada do programa deve conter uma class
     else:
         data = SF.program(l)
-        myProgram = data[0] 
-        showErrors(myProgram.err)
+        err = data[0].getErr() 
+        synTree   = data[3] 
+        typeList  = data[1][0]
+
+
+
+        return typeList, synTree, err
